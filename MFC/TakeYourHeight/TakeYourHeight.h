cdf68e32b04c3ea61790e42d3d64fd75292281e7
sdutwsl <wanghuilongsd@163.com>
2020-11-24 17:57:20 +0800

    增加MFC简易版本

---
+++ MFC/TakeYourHeight/TakeYourHeight.h
@@ -0,0 +1,32 @@
+﻿
+// TakeYourHeight.h: PROJECT_NAME 应用程序的主头文件
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "在包含此文件之前包含 'pch.h' 以生成 PCH"
+#endif
+
+#include "resource.h"		// 主符号
+
+
+// CTakeYourHeightApp:
+// 有关此类的实现，请参阅 TakeYourHeight.cpp
+//
+
+class CTakeYourHeightApp : public CWinApp
+{
+public:
+	CTakeYourHeightApp();
+
+// 重写
+public:
+	virtual BOOL InitInstance();
+
+// 实现
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CTakeYourHeightApp theApp;
